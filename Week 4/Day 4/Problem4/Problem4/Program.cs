addf8f7f9dd62f49f55dcb4aeb29f6d363d37d04
Aditi Singh <aditisingh291103@gmail.com>
2026-03-12 23:31:27 +0530

    Implemented E-Commerce Order Calculator using optional parameters in C# console application

---
+++ Week 4/Day 4/Problem4/Problem4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class OrderCalculator
+{
+    // Method with optional parameters
+    public static double CalculateFinalAmount(double price, int quantity, double discountPercent = 0, double shippingCharge = 50)
+    {
+        double subtotal = price * quantity;
+        double discount = subtotal * discountPercent / 100;
+        double amountAfterDiscount = subtotal - discount;
+        double finalAmount = amountAfterDiscount + shippingCharge;
+
+        Console.WriteLine("Subtotal: " + subtotal);
+        Console.WriteLine("Discount Applied: " + discount);
+        Console.WriteLine("Shipping Charge: " + shippingCharge);
+        Console.WriteLine("Final Amount: " + finalAmount);
+
+        return finalAmount;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Product Price: ");
+        double price = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Quantity: ");
+        int quantity = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\n--- Order with Default Discount and Shipping ---");
+        OrderCalculator.CalculateFinalAmount(price, quantity);
+
+        Console.WriteLine("\n--- Order with Discount Only ---");
+        OrderCalculator.CalculateFinalAmount(price, quantity, 10);
+
+        Console.WriteLine("\n--- Order with Discount and Custom Shipping ---");
+        OrderCalculator.CalculateFinalAmount(price, quantity, 10, 20);
+    }
+}
