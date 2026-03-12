4b30bbff67a07b98496b4ee87443215b31e8fdea
Aditi Singh <aditisingh291103@gmail.com>
2026-03-12 23:41:46 +0530

    Implemented recursive power calculator in C# console application

---
+++ Week 4/Day 4/Problem5/Problem5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class PowerCalculator
+{
+    // Recursive method
+    public static int CalculatePower(int baseNum, int exponent)
+    {
+        // Base case
+        if (exponent == 0)
+            return 1;
+
+        // Recursive call
+        return baseNum * CalculatePower(baseNum, exponent - 1);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Base: ");
+        int baseNum = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Exponent: ");
+        int exponent = Convert.ToInt32(Console.ReadLine());
+
+        if (exponent < 0)
+        {
+            Console.WriteLine("Exponent must be a positive integer.");
+            return;
+        }
+
+        int result = PowerCalculator.CalculatePower(baseNum, exponent);
+
+        Console.WriteLine("Base: " + baseNum);
+        Console.WriteLine("Exponent: " + exponent);
+        Console.WriteLine("Result: " + result);
+    }
+}
