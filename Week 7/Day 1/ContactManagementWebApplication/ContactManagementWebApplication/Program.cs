7f9bd0078576232252f229c719c40cf56540098c
Aditi Singh <aditisingh291103@gmail.com>
2026-04-01 00:17:09 +0530

    Contact management system

---
+++ Week 7/Day 1/ContactManagementWebApplication/ContactManagementWebApplication/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Contact}/{action=ShowContacts}/{id?}");
+
+app.Run();
