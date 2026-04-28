1bd80bf723b25b0048e2fba639e1661a586e7dc8
Aditi Singh <aditisingh291103@gmail.com>
2026-04-28 23:52:02 +0530

    implement contact form with template-driven validation and display list

---
+++ Week 11/Day 2/contact-management/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ContactFormComponent } from '../components/contact-form/contact-form.component';
+
+@NgModule({
+  declarations: [AppComponent, ContactFormComponent],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
